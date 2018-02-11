53e8def3f58b6f670bf8676774581bc7b70406b8
redoy.ahmed <redoyahmed@gmail.com>
2018-02-11 18:21:48 +0600

    second commit

--- app/src/main/java/com/example/redoyahmed/ezyperl/Activity/WelcomeScreen.java
+++ app/src/main/java/com/example/redoyahmed/ezyperl/Activity/WelcomeScreen.java
@@ -15,6 +15,7 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
 
             @Override
