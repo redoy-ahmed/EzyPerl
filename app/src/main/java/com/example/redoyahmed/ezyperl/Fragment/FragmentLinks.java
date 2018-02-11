5671d2c93b19ebcf4db4ef53906cb5cbbce00718
redoy.ahmed <redoyahmed@gmail.com>
2018-02-11 18:41:48 +0600

    second commit

---
+++ app/src/main/java/com/example/redoyahmed/ezyperl/Fragment/FragmentLinks.java
@@ -0,0 +1,26 @@
+package com.example.redoyahmed.ezyperl.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.redoyahmed.ezyperl.R;
+
+/**
+ * Created by redoy.ahmed on 11-Feb-2018.
+ */
+
+public class FragmentLinks extends Fragment {
+
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_links, null);
+
+        return rootView;
+    }
+}
